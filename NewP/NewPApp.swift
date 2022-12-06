e01b9b572f44f8312ddfac1c48a535146560bb8c
uzrivera <uzrivera@iu.edu>
2022-12-06 10:36:51 -0500

    Initial Commit

---
+++ NewP/NewPApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewPApp.swift
+//  NewP
+//
+//  Created by Uziel Rivera-Lopez on 12/6/22.
+//
+
+import SwiftUI
+
+@main
+struct NewPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
